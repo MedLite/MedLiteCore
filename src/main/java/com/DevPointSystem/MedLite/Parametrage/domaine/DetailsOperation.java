ad1ef420cb0958f5e448f5d727eb35193ca43776
Soufien AbdennadherDEV <soufienabdennadher54328@gmail.com>
2024-12-25 20:32:28 +0100

    firstCommit

---
+++ src/main/java/com/DevPointSystem/MedLite/Parametrage/domaine/DetailsOperation.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.DevPointSystem.MedLite.Parametrage.domaine;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.math.BigDecimal;
+import java.util.Date;
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
+
+/**
+ *
+ * @author Administrator
+ */
+@Entity
+@Table(name = "Details_Operation", schema = "param")
+@Audited
+@AuditTable("Details_Operation_AUD")
+public class DetailsOperation {
+    @EmbeddedId
+    protected DetailsOperationPK detailsOperationPK;
+
+    @MapsId("codeOperation")
+    @JoinColumn(name = "Code_Operation", referencedColumnName = "Code", nullable = false)
+    @ManyToOne(optional = false)
+    private Operation operation;
+
+    @Column(name = "User_Create", nullable = false, columnDefinition = "nvarchar(200)")
+    private String usercreate;
+
+    @JoinColumn(name = "Code_Type_Intervenant", referencedColumnName = "Code", nullable = false, insertable = false, updatable = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JsonBackReference
+    private TypeIntervenant typeIntervenant;
+
+    @Column(name = "Code_Type_Intervenant", insertable = false, updatable = false)
+    private Integer codeTypeIntervenant;
+
+    @Basic(optional = false)
+    @Column(name = "Date_Create", nullable = false, columnDefinition = "datetime default (getdate())")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateCreate;
+
+    @Column(name = "Montant", columnDefinition = ("decimal(18,3)"), nullable = false)
+    private BigDecimal montant;
+
+    public DetailsOperation() {
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getUsercreate() {
+        return usercreate;
+    }
+
+    public void setUsercreate(String usercreate) {
+        this.usercreate = usercreate;
+    }
+
+    public Date getDateCreate() {
+        return dateCreate;
+    }
+
+    public void setDateCreate(Date dateCreate) {
+        this.dateCreate = dateCreate;
+    }
+
+    public DetailsOperationPK getDetailsOperationPK() {
+        return detailsOperationPK;
+    }
+
+    public void setDetailsOperationPK(DetailsOperationPK detailsOperationPK) {
+        this.detailsOperationPK = detailsOperationPK;
+    }
+
+    public BigDecimal getMontant() {
+        return montant;
+    }
+
+    public void setMontant(BigDecimal montant) {
+        this.montant = montant;
+    }
+
+    public TypeIntervenant getTypeIntervenant() {
+        return typeIntervenant;
+    }
+
+    public void setTypeIntervenant(TypeIntervenant typeIntervenant) {
+        this.typeIntervenant = typeIntervenant;
+    }
+
+    public Integer getCodeTypeIntervenant() {
+        return codeTypeIntervenant;
+    }
+
+    public void setCodeTypeIntervenant(Integer codeTypeIntervenant) {
+        this.codeTypeIntervenant = codeTypeIntervenant;
+    }
+    
+}
